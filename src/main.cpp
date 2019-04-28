73f707692c60e0dec8494781b403feec442eb3de
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-04-28 23:27:12 +0200

    Build the master client

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include <boost/asio.hpp>
 
+#include "crypto/rsa_keypair.hpp"
+#include "master_client.hpp"
+
 int main(int argc, char* argv[])
 {
     if (argc < 4) {
@@ -12,6 +15,19 @@ int main(int argc, char* argv[])
     }
 
     boost::asio::io_service ioservice;
+
+    try {
+        MasterClient masterClient(
+            ioservice,
+            fs::path(argv[1]),
+            fs::path(argv[2]),
+            fs::path(argv[3])
+        );
+    } catch(const Dummy::Crypto::RSAKeyPairException& e) {
+        std::cerr << "Could not run the master client: " 
+            << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
     //GameServer gameServer(ioservice, 4648, argv[1]);
     //
     ioservice.run();
