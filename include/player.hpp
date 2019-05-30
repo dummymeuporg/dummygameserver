b99d9cf0143f17003c3c5a926d7d519bb95c4793
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-05-30 12:08:00 +0200

    Set Player server position

--- include/player.hpp
+++ include/player.hpp
@@ -16,8 +16,13 @@ public:
         return m_character;
     }
 
+    const std::pair<std::uint16_t, std::uint16_t>& serverPosition() const {
+        return m_serverPosition;
+    }
+
 private:
     std::shared_ptr<::GameSession> m_gameSession;
     std::shared_ptr<Dummy::Core::Character> m_character;
+    std::pair<std::uint16_t, std::uint16_t> m_serverPosition;
 
 };
